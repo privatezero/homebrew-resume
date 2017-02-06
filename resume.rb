f9d72b6bf863718761a4cff1968ea0636a11b54a
Andrew Weaver <weevz@uw.edu>
2017-02-06 16:34:00 -0500

    Update resume.rb

--- resume.rb
+++ resume.rb
@@ -6,10 +6,12 @@ class Resume < Formula
   url "https://github.com/privatezero/Resume/archive/0.0.01.zip"
   sha256 "10e68724df3e47dfd4bfff614021bded0a9e0220e4c20950743ba813e915df47"
   depends_on 'pandoc'
+  revision 1
 
  
   def install
   prefix.install 'README.md'
+  prefix.install 'Versions'
   bin.install 'pandoc_script'
   end
  
