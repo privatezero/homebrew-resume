62d06f9cab0e120e74c6e7d6b07dae9151a9aa1d
Andrew Weaver <weevz@uw.edu>
2017-03-24 10:12:47 -0400

    Update resume.rb

--- resume.rb
+++ resume.rb
@@ -7,7 +7,7 @@ class Resume < Formula
   head "https://github.com/privatezero/Resume.git"
   sha256 "368f8a4a58adb9e6aa2401a3d1e3712ce2f0f6d50b0013a911f6e7a3075aa380"
   depends_on 'pandoc'
-  revision 1
+  revision 2
 
  
   def install
@@ -19,7 +19,6 @@ class Resume < Formula
   
   def post_install
     system `echo "pandoc_script /usr/local/Cellar/resume/$(ls -t /usr/local/Cellar/resume | head -1)/README.md > /dev/null"`
-    system `echo "pandoc_script /usr/local/Cellar/resume/$(ls -t /usr/local/Cellar/resume | head -1)/versions/$(ls -t /usr/local/Cellar/resume/$(ls -t /usr/local/Cellar/resume | head -1)/versions | head -1) > /dev/null"`
   end
  
 end
