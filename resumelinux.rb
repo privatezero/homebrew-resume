d65b6e1d4880c6424a71ed7f507c79f073985bb0
Andrew Weaver <weevz@uw.edu>
2017-05-07 13:48:03 -0700

    Update resumelinux.rb

--- resumelinux.rb
+++ resumelinux.rb
@@ -1,4 +1,4 @@
-class Resume < Formula
+class Resumelinux < Formula
   url "https://github.com/privatezero/Resume/archive/0.0.02.zip"
   head "https://github.com/privatezero/Resume.git"
   sha256 "368f8a4a58adb9e6aa2401a3d1e3712ce2f0f6d50b0013a911f6e7a3075aa380"
